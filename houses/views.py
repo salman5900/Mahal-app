0b71857d1647d42d1a7047ad5ccc476364315353
Salman Ahamed <salmanahamed5900@gmail.com>
2025-07-07 18:38:48 +0000

    home page page almost done

--- houses/views.py
+++ houses/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Hosuse
 
 # Create your views here.
 def houses(request):
-    return render(request, 'Houses/houses.html',{"transparent_nav": False})
+    houses = Hosuse.objects.all().order_by('-date_posted')
+    context = {
+        'houses': houses,
+        'transparent_nav': False,
+    }
+    return render(request, 'Houses/houses.html', context)
